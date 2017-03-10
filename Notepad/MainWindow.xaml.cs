3e0195e3fa4b5787c0efef7ef353c0c44a7ae525
fiki574 <fiki8796@hotmail.com>
2017-03-10 18:13:38 +0100

    Initial commit

---
+++ Notepad/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+
+namespace Notepad
+{
+    public partial class MainWindow : Window
+    {
+        public static TextEditorHandler handler;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextEditor.handler = new MainWindowHandler(this);
+            Content = new TextEditor();
+        }
+
+        public void TextData_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            handler.Resize(e.NewSize);
+        }
+
+        public void ChangeTitle(string title)
+        {
+            Title = title;
+        }
+    }
+
+    public class TextEditorHandler
+    {
+        private TextEditor window;
+
+        public TextEditorHandler(TextEditor window)
+        {
+            this.window = window;
+        }
+
+        public void Resize(Size s)
+        {
+            window.Resize(s);
+        }
+    }
+}
---
+++ Notepad/TextEditor.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notepad
+{
+    public partial class TextEditor : UserControl
+    {
+        public static MainWindowHandler handler;
+
+        public TextEditor()
+        {
+            InitializeComponent();
+            MainWindow.handler = new TextEditorHandler(this);
+            TextData.Width = 725;
+            TextData.Height = 550;
+        }
+
+        public void Resize(Size s)
+        {
+            TextData.Width = s.Width;
+            TextData.Height = s.Height;
+            Width = s.Width;
+            Height = s.Height;
+        }
+    }
+
+    public class MainWindowHandler
+    {
+        private MainWindow window;
+
+        public MainWindowHandler(MainWindow window)
+        {
+            this.window = window;
+        }
+
+        public void ChangeTitle(string title)
+        {
+            window.ChangeTitle(title);
+        }
+    }
+}
